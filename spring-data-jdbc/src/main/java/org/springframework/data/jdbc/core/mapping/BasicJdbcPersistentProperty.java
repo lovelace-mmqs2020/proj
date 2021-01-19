f835a1aba1b2717c09e45b2424ecf0bcac1f591d
alexmik <alexmik95@gmail.com>
2021-01-19 20:32:39 +0100

    #82984

--- spring-data-jdbc/src/main/java/org/springframework/data/jdbc/core/mapping/BasicJdbcPersistentProperty.java
+++ spring-data-jdbc/src/main/java/org/springframework/data/jdbc/core/mapping/BasicJdbcPersistentProperty.java
@@ -36,22 +36,6 @@ public class BasicJdbcPersistentProperty extends BasicRelationalPersistentProper
 	 * @param property must not be {@literal null}.
 	 * @param owner must not be {@literal null}.
 	 * @param simpleTypeHolder must not be {@literal null}.
-	 * @param context must not be {@literal null}
-	 * @deprecated since 2.0, use
-	 *             {@link #BasicJdbcPersistentProperty(Property, PersistentEntity, SimpleTypeHolder, NamingStrategy)}.
-	 */
-	@Deprecated
-	public BasicJdbcPersistentProperty(Property property, PersistentEntity<?, RelationalPersistentProperty> owner,
-			SimpleTypeHolder simpleTypeHolder, RelationalMappingContext context) {
-		super(property, owner, simpleTypeHolder, context);
-	}
-
-	/**
-	 * Creates a new {@link BasicJdbcPersistentProperty}.
-	 *
-	 * @param property must not be {@literal null}.
-	 * @param owner must not be {@literal null}.
-	 * @param simpleTypeHolder must not be {@literal null}.
 	 * @param namingStrategy must not be {@literal null}
 	 * @since 2.0
 	 */
